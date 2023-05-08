a9b7f45561f208df4dbd09720566a2b05cfe107f
Lauro Guedes <leowgweb@gmail.com>
2023-05-08 17:06:35 -0300

    feat(lib): install lib dropdown headlessui

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
-
 import tailwind from "@astrojs/tailwind";
+
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), tailwind()]
+  integrations: [preact(), tailwind(), vue()]
 });
